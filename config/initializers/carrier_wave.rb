d532b65d7d02c0f9e8b511d560acdeb7daec9f86
James Kelsey <jamesmskelsey@gmail.com>
2015-10-04 12:53:53 -0700

    set fog-aws region to us-west-2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Config for AMAZON S3
-      provider:               => 'AWS',
-      aws_access_key_id:      => ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key:  => ENV['S3_SECRET_KEY'],
-      #region: 'Oregon'
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_ke:  => ENV['S3_SECRET_KEY'],
+      :region                 => 'us-west-2'
     }
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.fog_directory      =  ENV['S3_BUCKET']
