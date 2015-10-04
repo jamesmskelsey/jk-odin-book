3492a108769f07de15d17d5424928bba137315ac
James Kelsey <jamesmskelsey@gmail.com>
2015-10-04 07:20:36 -0700

    add pictures to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:content, :author_id)
+      params.require(:post).permit(:content, :author_id, :picture)
     end
 
     def correct_user
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,19 @@
 class Post < ActiveRecord::Base
   validates :content, presence: true, length: {maximum: 200}
+  validate  :picture_size
   default_scope -> {order(created_at: :desc)}
   belongs_to :author, class_name: "User"
 
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
+
+  mount_uploader :picture, PictureUploader
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
