7f1cfc19b86d04ef7bef9ef4d25195c5e19df217
James Kelsey <jamesmskelsey@gmail.com>
2015-10-04 08:29:49 -0700

    more aws fixes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,22 +2,14 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Config for AMAZON S3
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY']
+      provider:               => 'AWS',
+      aws_access_key_id:      => ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key:  => ENV['S3_SECRET_KEY'],
+      #region: 'Oregon'
     }
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.fog_directory      =  ENV['S3_BUCKET']
-  end
-end
-
-if Rails.env.development?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Config for AMAZON S3
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET_KEY']
-    }
-    config.fog_directory      =  ENV['S3_BUCKET']
+    config.fog_public = false
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   end
 end
